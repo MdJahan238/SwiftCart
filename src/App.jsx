fff2100d599bc1bcf665ffc72ab6c64f75df1c27
Md Jahan <74582430+MdJahan238@users.noreply.github.com>
2024-12-02 02:23:29 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Header from './component/Shared/Header/Header'
-import About from './component/About/about'
-import Contact from './component/Contact/Contact'
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <About></About>
-      <Contact></Contact>
+     
     </div>
   )
 }
