29f64000941680736315e94b9760abe23aada826
mdjahan162 <mdjahan162@gmail.com>
2024-12-02 02:57:41 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from './component/Home/Home';
 import About from './component/About/About';
-
-
-=======
-
 import './App.css'
->>>>>>> fff2100d599bc1bcf665ffc72ab6c64f75df1c27
+
 
 function App() {
   const router = createBrowserRouter([
@@ -30,18 +25,13 @@ function App() {
   ]);
 
   return (
-<<<<<<< HEAD
+
   <div>
  <RouterProvider router={router}></RouterProvider>
   </div>
 )
  
-=======
-    <div>
-     
-    </div>
-  )
->>>>>>> fff2100d599bc1bcf665ffc72ab6c64f75df1c27
+
 }
 
 export default App
