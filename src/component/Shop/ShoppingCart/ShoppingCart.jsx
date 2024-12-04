1987459730793ce935dc65c67739252af4a92a4c
mdjahan162 <mdjahan162@gmail.com>
2024-12-04 08:56:42 +0600

    checking

--- src/component/Shop/ShoppingCart/ShoppingCart.jsx
+++ src/component/Shop/ShoppingCart/ShoppingCart.jsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { faArrowRight, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const ShoppingCart = ({cart,handleClearCart}) => {
