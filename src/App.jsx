1e86fa6d115ccc567235b8526c4634e9332d8a63
mdjahan162 <mdjahan162@gmail.com>
2024-12-05 22:59:04 +0600

    layout update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './component/Home/Home';
-import About from './component/About/About';
+import About from './component/About/about';
 import './App.css'
 import Contact from './component/Contact/Contact';
 import Shop from './component/Shop/Shop';
